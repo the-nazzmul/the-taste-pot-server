e3b51b4df04bfe0fc7176d57dfabf384fa53e8d3
Nazmul Hussain <nazmul.hussain.utchash@gmail.com>
2023-06-09 01:33:48 +0600

    added save user api

--- index.js
+++ index.js
@@ -11,10 +11,64 @@ const port = process.env.PORT || 4000;
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wy9csda.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const userCollection = client.db('tastePot').collection('users')
+
+        // user related apis
+        app.get('/users', async (req, res) => {
+            res.send('users here')
+        })
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const query = {email: user.email}
+            const existingUser = await userCollection.findOne(query)
+
+            if(existingUser){
+                res.send({message: 'user already exists'})
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
     res.send('Pot is boiling')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Pot is boiling on port:', port);
 })
